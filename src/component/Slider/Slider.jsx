8db99f63c9400dffb729bbefa528bf9cf01d0525
gorrec41 <batrak1@ya.ru>
2021-08-13 13:34:36 +0300

    fix bag list slider

--- src/component/Slider/Slider.jsx
+++ src/component/Slider/Slider.jsx
@@ -3,32 +3,36 @@ import cn from 'classnames'
 import st from './Slider.module.css'
 function Slider() {
     const slider = useRef(null)
-    const [slid__items, setItems] = useState([1, 2, 3, 4, 5,6, 7, 8, 9, 0])
-    const [prev, setPrev] = useState(false)
-    const [next, setNext] = useState(false)
+    const [slid__items, setItems] = useState([1,2,3])
     let position = 0
+    const art=['1Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum incidunt repellat, accusamus eaque quibusdam asperiores blanditiis voluptas saepe, laudantium dignissimos id quos fuga molestiae cumque provident quae, eos accusantium at.','2Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum incidunt repellat, accusamus eaque quibusdam asperiores blanditiis voluptas saepe, laudantium dignissimos id quos fuga molestiae cumque provident quae, eos accusantium at.','3Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum incidunt repellat, accusamus eaque quibusdam asperiores blanditiis voluptas saepe, laudantium dignissimos id quos fuga molestiae cumque provident quae, eos accusantium at.']
+
     const prevHandler = () => {
-        if (position <0 ){
-            setNext(false)
+        if (position ===0 ||position>0){
+            position=0
+            slider.current.childNodes.forEach((elem) => {
+                elem.style=`transform: translateX(${position}px)`
+            })
+        } else {
             position +=300
             slider.current.childNodes.forEach((elem) => {
                 elem.style=`transform: translateX(${position}px)`
             })
-        } else {
-           console.log("dd")
-            setPrev(true)
         }
+        
     }
     const nextHandler = () => {
-        if (position === -(slid__items.length -1) * 300) {
-            setNext(true)
-        } else {
-            setPrev(false)
-            position -=300
+       if(position === -((slid__items.length -1) * 300)||position<-((slid__items.length -1) * 300)){
+            position = -((slid__items.length -1) * 300)
             slider.current.childNodes.forEach((elem) => {
                 elem.style=`transform: translateX(${position}px)`
-            })
-        }
+                })}
+                else{
+                position -=300
+                slider.current.childNodes.forEach((elem) => {
+                    elem.style=`transform: translateX(${position}px)`
+                    })
+            }
     }
     return (
         <section className={st.slider } id='1234'>
@@ -38,12 +42,12 @@ function Slider() {
                         {slid__items.map(() => {
                             return(
                             <div className={st.slid__item}>
-
+                               
                             </div>)
                     })}
                     </div>
-                    <button className={cn(st.slid__button, st.button__prev)} disabled={prev} onClick={prevHandler}>{'<'}</button>
-                    <button className={cn(st.slid__button, st.button__next)} disabled={next} onClick={nextHandler}>{'>'}</button>
+                    <button className={cn(st.slid__button, st.button__prev)} onClick={prevHandler}>{'<'}</button>
+                    <button className={cn(st.slid__button, st.button__next)}  onClick={nextHandler}>{'>'}</button>
                 </div>
             </div>
         </section>
